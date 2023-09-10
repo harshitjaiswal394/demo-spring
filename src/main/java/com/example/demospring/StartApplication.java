b0837eecd9532126ac854c9d189a049479012cc2
harshitjaiswal394 <harshitjaiswal394@hotmail.com>
2023-09-10 20:06:49 +0530

    Final commit

---
+++ src/main/java/com/example/demospring/StartApplication.java
@@ -0,0 +1,25 @@
+package com.example.demospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Howdy Folks..Welcome to Devops Coaching by Coach Harshit!");
+        model.addAttribute("msg", "Hello All..We are deploying springboot application into EKS cluster using Helm + Jenkins Pipeline!!!! Today is Sep 10th, 2023");
+        model.addAttribute("msg","WELCOME TO DEVOPS");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
